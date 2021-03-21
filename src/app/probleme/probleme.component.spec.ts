14e870a8a4f049aa291aec229038d4aadc7e0e60
Jean-Claude Chiniara <j-c.chiniara@hotmail.com>
2021-03-21 15:41:44 -0400

    TESTS #1 | Zone PRÉNOM invalide avec 2 caractères

---
+++ src/app/probleme/probleme.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ProblemeComponent } from './probleme.component';
+
+describe('ProblemeComponent', () => {
+  let component: ProblemeComponent;
+  let fixture: ComponentFixture<ProblemeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports : [ReactiveFormsModule],
+      declarations: [ ProblemeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProblemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('#1 | Zone PRÉNOM invalide avec 2 caractères', () => {
+    let zonePRENOM = component.problemeForm.controls['Prenom'];
+    zonePRENOM.setValue('a'.repeat(3))
+    expect(zonePRENOM.valid).toBeTruthy();
+  });
+
+});
