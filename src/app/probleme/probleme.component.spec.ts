5011157fc7041e85cc58155185e471ef2dd97adc
Jean-Claude Chiniara <j-c.chiniara@hotmail.com>
2021-03-21 22:27:33 -0400

    fait #4 | Zone PRÉNOM invalide avec aucune valeur

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -27,23 +27,32 @@ describe('ProblemeComponent', () => {
 
   it('#1 | Zone PRÉNOM invalide avec 2 caractères', () => {
     let errors = {};
-    let zonePRENOM = component.problemeForm.get('prenom');
+    let zonePRENOM = component.problemeForm.get('Prenom');
     zonePRENOM.setValue('a'.repeat(2))
     errors = zonePRENOM.errors || {};
     expect(errors['minlength']).toBeTruthy();
   });
 
   it('#2 | Zone PRÉNOM valide avec 3 caractères', () => {
-    let zonePRENOM = component.problemeForm.controls['prenom'];
+    let zonePRENOM = component.problemeForm.controls['Prenom'];
     zonePRENOM.setValue('a'.repeat(3))
     expect(zonePRENOM.valid).toBeTruthy();
   });
 
   it('#3 | Zone PRÉNOM valide avec 200 caractères', () => {
-    let zonePRENOM = component.problemeForm.controls['prenom'];
+    let zonePRENOM = component.problemeForm.controls['Prenom'];
     zonePRENOM.setValue('a  '.repeat(1))
     expect(zonePRENOM.valid).toBeTruthy();
   });
+
+  it('#4 | Zone PRÉNOM invalide avec aucune valeur', () => {
+    let errors = {};
+    let zonePRENOM = component.problemeForm.get('Prenom');
+    zonePRENOM.setValue('')
+    errors = zonePRENOM.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+
   
   
 });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'Inter-probleme',
+  selector: 'app-probleme',
   templateUrl: './probleme.component.html',
   styleUrls: ['./probleme.component.css']
 })
 export class ProblemeComponent implements OnInit {
   problemeForm: FormGroup;
+  
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.problemeForm = this.fb.group({
-        prenom: ['',[Validators.minLength(3)]]
+        Prenom: ['',[Validators.minLength(3), Validators.required]]
     });
   }
 
